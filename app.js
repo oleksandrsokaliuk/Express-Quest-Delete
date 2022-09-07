460153fc57e9aa9823129e5bcf5457550d12fb49
oleksandrsokaliuk <oleksandr-sokaliuk_student2022@wilder.school>
2022-09-07 13:11:26 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to the list of users");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
