93ffe23f572a6c49401c0808036e1fe3f763926a
oleksandrsokaliuk <oleksandr-sokaliuk_student2022@wilder.school>
2022-09-07 17:56:57 +0200

    added solution to DELETE query quest

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.post("/api/users", userHandlers.postUser);
 app.put("/api/movies/:id", userHandlers.updateMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
 
+app.delete("/api/movies/:id", userHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- userHandlers.js
+++ userHandlers.js
@@ -164,6 +164,42 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
@@ -173,4 +209,6 @@ module.exports = {
   postUser,
   updateMovie,
   updateUser,
+  deleteMovie,
+  deleteUser,
 };
